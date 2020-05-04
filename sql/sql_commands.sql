e6f96686efa4c1a7f00fc4cf5a86d1e3aa4e7572
Chippin02 <joserippinrodriguez@gmail.com>
2020-05-04 17:18:34 +0200

    estructura de ficheros definida

--- sql/sql_commands.sql
+++ sql/sql_commands.sql
@@ -1,9 +1,13 @@
 CREATE DATABASE M08_ToDo;
+
 CREATE USER 'ToDoUser'@'localhost' IDENTIFIED BY 'linuxlinux';
+
 USE M08_ToDo;
+
 CREATE TABLE tasks (
     id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     task varchar(255) NOT NULL,
     date timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
 GRANT ALL PRIVILEGES ON M08_ToDo.* TO 'ToDoUser'@'localhost';
