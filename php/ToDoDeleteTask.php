2234b596291a46cb507e1cde7cdf36a613809e5e
Chippin02 <joserippinrodriguez@gmail.com>
2020-05-04 18:51:30 +0200

    logica hecha, falla crear sin nada y eliminar - REVISAR

---
+++ php/ToDoDeleteTask.php
@@ -0,0 +1,11 @@
+<?php
+    include('DB_Connection.php');
+
+    $id = $_REQUEST['id'];
+
+    $query = $dbconn->prepare('DELETE FROM tasks WHERE id = :id;');
+    $query->bindParam(":id", $id, PDO::PARAM_STR);
+    $query->execute();
+
+    header ("Refresh:0; url=../html/index.html");
+?>
